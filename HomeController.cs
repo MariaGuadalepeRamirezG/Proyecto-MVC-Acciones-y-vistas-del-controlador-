a1cea0693506fa975de39eb0b013f6c999e497a6
MariaGuadalepeRamirezG <71472812+MariaGuadalepeRamirezG@users.noreply.github.com>
2021-03-20 21:39:04 -0600

    Controllers

---
+++ HomeController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+   public class HomeController : Controller
+{
+    [Route("")]
+    [Route("Index")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return ControllerContext.MyDisplayRouteInfo();
+    }
+
+    [Route("About")]
+    public IActionResult About()
+    {
+        return ControllerContext.MyDisplayRouteInfo();
+    }
+}
+public class MyDemoController : Controller
+{
+    [Route("")]
+    [Route("Home")]
+    [Route("Home/Index")]
+    [Route("Home/Index/{id?}")]
+    public IActionResult MyIndex(int? id)
+    {
+        return ControllerContext.MyDisplayRouteInfo(id);
+    }
+
+    [Route("Home/About")]
+    [Route("Home/About/{id?}")]
+    public IActionResult MyAbout(int? id)
+    {
+        return ControllerContext.MyDisplayRouteInfo(id);
+    }
+}
+public class MyDemo2Controller : Controller
+{
+    [Route("/articles/{page}")]
+    public IActionResult ListArticles(int page)
+    {
+        return ControllerContext.MyDisplayRouteInfo(page);
+    }
+}
+[Route("api/[controller]")]
+[ApiController]
+public class Test2Controller : ControllerBase
+{
+    [HttpGet]   // GET /api/test2
+    public IActionResult ListProducts()
+    {
+        return ControllerContext.MyDisplayRouteInfo();
+    }
+
+    [HttpGet("{id}")]   // GET /api/test2/xyz
+public IActionResult GetProduct(string id)
+{
+   return ControllerContext.MyDisplayRouteInfo(id);
+}
+
+    [HttpGet("int/{id:int}")] // GET /api/test2/int/3
+public IActionResult GetIntProduct(int id)
+{
+    return ControllerContext.MyDisplayRouteInfo(id);
+}
+
+    [HttpGet("int2/{id}")]  // GET /api/test2/int2/3
+public IActionResult GetInt2Product(int id)
+{
+    return ControllerContext.MyDisplayRouteInfo(id);
+}
+}
+[ApiController]
+public class MyProductsController : ControllerBase
+{
+    [HttpGet("/products3")]
+    public IActionResult ListProducts()
+    {
+        return ControllerContext.MyDisplayRouteInfo();
+    }
+
+    [HttpPost("/products3")]
+    public IActionResult CreateProduct(MyProduct myProduct)
+    {
+        return ControllerContext.MyDisplayRouteInfo(myProduct.Name);
+    }
+}
+[ApiController]
+public class Products2ApiController : ControllerBase
+{
+    [HttpGet("/products2/{id}", Name = "Products_List")]
+    public IActionResult GetProduct(int id)
+    {
+        return ControllerContext.MyDisplayRouteInfo(id);
+    }
+}
+[ApiController]
+[Route("products")]
+public class ProductsApiController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult ListProducts()
+    {
+        return ControllerContext.MyDisplayRouteInfo();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetProduct(int id)
+    {
+        return ControllerContext.MyDisplayRouteInfo(id);
+    }
+}
+[Route("Home")]
+
+}
